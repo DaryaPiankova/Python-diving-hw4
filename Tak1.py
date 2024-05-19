1c8516e4951bca56c9a5a98ca357c190e065ed2c
Dasha <darya.piankova@yandex.ru>
2024-05-19 21:22:09 +0500

    init commit

---
+++ Tak1.py
@@ -0,0 +1,12 @@
+def matrix_transpose(m):
+    """возвращает транспонированную матрицу"""
+    new = []
+    for i in range(len(m[0])):
+        new.append([])
+    for i in range(len(m)):
+        for j in range(len(m[i])):
+            new[j].append(m[i][j])
+    return new
+
+
+print(matrix_transpose([[1, 2], [3, 4], [5, 6]]))
